e89faa3a2dd05711567b4bda4db540f1603f1700
Lakym972 <132657810+Lakym972@users.noreply.github.com>
2024-01-10 10:31:51 +0100

    ajout listing

--- src/components/listing/index.jsx
+++ src/components/listing/index.jsx
@@ -1,44 +1,39 @@
 import Button from "../common/Button.jsx"
 import Typography from "../common/Typography.jsx"
+import { useEffect, useState } from "react"
 
 
 function Listing() {
 
-    const listing = [
-        {
-            img : "./src/assets/img/right.png",
-            title : "The Dandy chair",
-            price : "£250"
-        },
-        {
-            img : "./src/assets/img/vases.png",
-            title : "Rustic Vase Set",
-            price : "£155"
-        },
-        {
-            img : "./src/assets/img/bottle.png",
-            title : "The Silky Vase",
-            price : "£125"
-        },
-        {
-            img : "./src/assets/img/lampe.png",
-            title : "The Lucy Lamp",
-            price : "£399"
+    const [items, setItems] = useState([])
+
+    useEffect(()=> {
+        const data = async () => {
+            const response = await fetch('https://passerelle-shop-api.julienpoirier-webdev.com/products');
+            const results = await response.json();
+            setItems(results);
+
+            console.log(results);
         }
-    ]
+        data();
+    },[]);
 
     return (
         <>
         <Typography customClasses="mt-[5rem] mb-[2rem]" tag="h2">New ceramics</Typography>
 
         <div className="flex justify-between">{
-            listing.map(list =>
-            <div key={list}>
-                <img className="w-[19rem] h-[23rem] mb-[1.5rem]" src={list.img} alt="image produit" />
-                <Typography customClasses="mb-[0.5rem]" tag="h4">{list.title}</Typography>
-                <p>{list.price}</p>
-            </div>
-            )}
+            items.length > 0 ? items.map((item) => {
+                return(
+                <div key={item}>
+                    <img className="w-[19rem] h-[23rem]" src={item.mainImageURL} alt="figurine" />
+                    <Typography tag="h4">{item.name}</Typography>
+                    <p>{item.price}</p>
+                </div>
+                )
+            }) 
+            : null
+        }
         </div>
         <div className="flex justify-center mt-[3rem]">
         <Button variant="primary">View collection</Button>
